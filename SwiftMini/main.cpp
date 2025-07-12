cf92f157fc92650aa8f6a8e4ab968c230ad7878b
Kamil Khairullin <kamil.khayrullin.personal@yandex.ru>
2025-07-12 12:59:46 +0300

    Initial Commit

---
+++ SwiftMini/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SwiftMini
+//
+//  Created by Kamil on 12.07.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
